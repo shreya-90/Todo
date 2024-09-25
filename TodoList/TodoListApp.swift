bbe2455d91cb2d3ec46a80eec24b56d9739a0b47
Shreya Pallan <shreyapallan@Shreyas-MBP.lan>
2024-09-25 14:07:38 +0530

    Initial project setup

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Shreya Pallan on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
