9e1e9d811a9073ee84535c1c8b59849c3623c673
Shreya Pallan <shreyapallan@Shreyas-MBP.lan>
2024-09-26 13:36:50 +0530

    Adding a View Model to seperate data and logic from View

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,19 +9,15 @@ import SwiftUI
 
 struct ListView: View {
     
-     @State var items: [ItemModel] = [
-        ItemModel(title: "This is the first title!", isCompleted: false),
-        ItemModel(title: "This is the second!", isCompleted: true),
-        ItemModel(title: "Third", isCompleted: false)
-     ]
+    @EnvironmentObject var listViewModel: ListViewModel
     
     var body: some View {
         List {
-            ForEach(items) { item in
+            ForEach(listViewModel.items) { item in
                 ListRowView(item: item)
                
-            }.onDelete(perform: deleteItem)
-            .onMove(perform: moveItem)
+            }.onDelete(perform: listViewModel.deleteItem)
+                .onMove(perform: listViewModel.moveItem)
             
         }.navigationTitle("Todo 📝")
             .navigationBarItems(
@@ -31,21 +27,13 @@ struct ListView: View {
             .listStyle(.plain)
     }
     
-    
-    func deleteItem(indexSet: IndexSet) {
-        items.remove(atOffsets: indexSet)
-    }
-    
-    func moveItem(from: IndexSet, to: Int) {
-        items.move(fromOffsets: from, toOffset: to)
-    }
-    
 }
 
 #Preview {
     NavigationView {
         ListView()
     }
+    .environmentObject(ListViewModel())
 }
 
 
